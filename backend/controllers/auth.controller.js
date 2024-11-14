8547a8b19aea52a9fad65d2ae3fce8bcad92dc8c
faysal-islam-fd <fahadiislam01@gmail.com>
2024-11-14 07:53:16 +0600

    login implemented

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -24,3 +24,26 @@ export const register = async (req, res) => {
     }
     
 }
+
+export const login = async (req, res) => {
+
+    const {email,password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({success:false,message:"All fields are required"})
+    }
+    try{
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({success:false,message:"User not found"})
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({success:false,message:"Wrong password"})
+        }
+        generateToken(user._id,res)
+        res.status(200).json({success:true,message:"User logged in successfully"})
+
+    }catch(error){
+        res.status(500).json({success:false,message:"Internal Server Error"})
+    }
+}
