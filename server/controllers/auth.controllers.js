9930da8469ebffee8870ed22b2f500ee5f27a313
faysal-islam-fd <fahadiislam01@gmail.com>
2024-11-22 10:32:04 +0600

    used bcryptjs to hash password

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,5 +1,5 @@
 import User from "../models/user.models.js";
-
+import bcrypt from "bcryptjs";
 
 export const register  = async(req,res) => {
     try{
@@ -11,11 +11,14 @@ export const register  = async(req,res) => {
         if(userExist){
             return res.status(400).json({error: "Email already exists"})
         }
-        const user = new User({name, email, password});
+        const hashedPassword = await bcrypt.hash(password, 8);
+        const user = new User({name, email, password: hashedPassword});
         await user.save();
         res.status(201).json({success: true,message: "User registered successfully"})
     }
     catch(err){
+        console.log(err.message);
+        
         res.status(500).json({error: "Internal server error"})
     }
 }
